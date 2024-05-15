98843813f63209ab855d00a72829a7a3019c6be5
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-16 02:45:10 +0300

    Installed ShadCDN's Button Component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,52 @@
+import Link from "next/link";
+import Image from "next/image";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
 const Header = () => {
-	return <header>Header</header>;
+	return (
+		<NavigationMenu className="w-100 max-w-none shadow-md flex items-center justify-between p-4">
+			<Image src={"/logo.svg"} width={240} height={45.36} alt="Logo" />
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<Link href="/home" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={navigationMenuTriggerStyle()}
+						>
+							Home
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link href="/home" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={navigationMenuTriggerStyle()}
+						>
+							Products
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link href="/home" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={navigationMenuTriggerStyle()}
+						>
+							Cart
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
+			</NavigationMenuList>
+		</NavigationMenu>
+	);
 };
 
 export default Header;
