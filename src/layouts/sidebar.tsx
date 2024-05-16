70265eedb072d393565881437794d60e62ddf1e3
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-16 04:30:56 +0300

    Completed The Sidebar Component

--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import {
 	Sheet,
 	SheetContent,
-	SheetDescription,
 	SheetHeader,
-	SheetTitle,
 	SheetFooter,
 	SheetClose,
 	SheetTrigger,
 } from "@/components/ui/sheet";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { navLinks, navButtons } from "@/constants/constants";
 import { ChildrenType } from "@/types/types";
 
 const Sidebar = ({ children }: ChildrenType) => {
 	return (
 		<Sheet>
 			<SheetTrigger asChild>{children}</SheetTrigger>
-			<SheetContent side={"right"}>
-				<SheetHeader>
+			<SheetContent side={"right"} className="flex flex-col gap-4">
+				<SheetHeader className="px-4">
 					<Image
 						src={"/logo.svg"}
 						width={160}
@@ -24,15 +27,46 @@ const Sidebar = ({ children }: ChildrenType) => {
 						priority
 						alt="Logo"
 					/>
+					<Separator />
 				</SheetHeader>
-				<div className="grid gap-4 py-4">
-					<div className="grid grid-cols-4 items-center gap-4">
-						Name
-					</div>
-					<div className="grid grid-cols-4 items-center gap-4">
-						Username
-					</div>
-				</div>
+				<ul className="w-100 flex flex-col gap-4">
+					{navLinks.map((link, index) => (
+						<li
+							key={index}
+							className={
+								"min-w-full text-left font-bold" +
+								navigationMenuTriggerStyle()
+							}
+						>
+							<Link
+								href={`/${link.toLowerCase()}`}
+								className="w-full"
+							>
+								{link}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<Separator />
+				<ul className="w-100 flex flex-col gap-4">
+					{navButtons.map((button, index) => (
+						<li key={index} className={"min-w-full text-left"}>
+							<Button
+								variant={
+									button === "Login" ? "outline" : "default"
+								}
+								className={
+									button === "Login"
+										? navigationMenuTriggerStyle() +
+										  " min-w-full text-left"
+										: "min-w-full text-left"
+								}
+							>
+								{button}
+							</Button>
+						</li>
+					))}
+				</ul>
 				<SheetFooter>
 					<SheetClose asChild>Save changes</SheetClose>
 				</SheetFooter>
