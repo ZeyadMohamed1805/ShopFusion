330854ab313bf69e81b8740e186b19cb20424cce
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-16 03:20:54 +0300

    Completed The Header Component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,21 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import {
 	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuIndicator,
 	NavigationMenuItem,
 	NavigationMenuLink,
 	NavigationMenuList,
-	NavigationMenuTrigger,
-	NavigationMenuViewport,
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
 const Header = () => {
 	return (
-		<NavigationMenu className="w-100 max-w-none shadow-md flex items-center justify-between p-4">
-			<Image src={"/logo.svg"} width={240} height={45.36} alt="Logo" />
+		<NavigationMenu className="w-100 max-w-none shadow flex items-center justify-between p-4">
+			<Image src={"/logo.svg"} width={160} height={30.24} alt="Logo" />
 			<NavigationMenuList>
 				<NavigationMenuItem>
 					<Link href="/home" legacyBehavior passHref>
@@ -43,6 +40,17 @@ const Header = () => {
 							Cart
 						</NavigationMenuLink>
 					</Link>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Button
+						variant={"outline"}
+						className={navigationMenuTriggerStyle()}
+					>
+						Login
+					</Button>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Button>Register</Button>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
