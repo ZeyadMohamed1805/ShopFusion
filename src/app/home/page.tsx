e5f81122a4727690e7d71f53a62e24b65223fb62
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-17 14:18:52 +0300

    Initalized The Hero Component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
 import Carousel from "@/components/home/carousel";
 
 const Home = () => (
-	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8">
+	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex flex-col items-center">
 		<Carousel />
 	</main>
 );
--- src/components/home/carousel.tsx
+++ src/components/home/carousel.tsx
@@ -1,5 +1,34 @@
-const Carousel = () => {
-	return <div>Carousel</div>;
+import { Card, CardContent } from "@/components/ui/card";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
+const carousel = () => {
+	return (
+		<Carousel className="w-full max-w-xl">
+			<CarouselContent>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<CarouselItem key={index}>
+						<div className="p-1">
+							<Card>
+								<CardContent className="flex aspect-square items-center justify-center p-6">
+									<span className="text-4xl font-semibold">
+										{index + 1}
+									</span>
+								</CardContent>
+							</Card>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
+	);
 };
 
-export default Carousel;
+export default carousel;
