df7d48fa5e5162f01f2e050b1737689a308a43bf
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-17 14:01:30 +0300

    Added Carousel Component To The Home Page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,8 @@
+import Carousel from "@/components/home/carousel";
+
 const Home = () => (
-	<main className="w-full min-h-[calc(100vh-200.2px)] flex items-center justify-center">
-		<h1 className="text-3xl fw-bolder">Home</h1>
+	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8">
+		<Carousel />
 	</main>
 );
 
