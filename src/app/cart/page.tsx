b8d48a519b4e859e1dc8f4e7ec863a58542dbed4
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-17 13:57:25 +0300

    Adjusted The Min Heights Of The Pages

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,5 @@
 const Cart = () => (
-	<main className="w-full min-h-screen flex items-center justify-center">
+	<main className="w-full min-h-[calc(100vh-200.2px)] flex items-center justify-center">
 		<h1 className="text-3xl fw-bolder">Cart</h1>
 	</main>
 );
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-	<main className="w-full min-h-screen flex items-center justify-center">
+	<main className="w-full min-h-[calc(100vh-200.2px)] flex items-center justify-center">
 		<h1 className="text-3xl fw-bolder">Home</h1>
 	</main>
 );
