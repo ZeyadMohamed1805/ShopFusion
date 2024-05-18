245a0017120d2322dd20be3dfb30c42ff0138053
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-19 02:13:05 +0300

    Added The Sidenav Layout Component To The Layouts

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import Sidenav from "./sidenav";
 import Header from "./Header";
 import Footer from "./footer";
 import { Toaster } from "@/components/ui/toaster";
 import { ChildrenType } from "@/types/types";
 
 const Layout = ({ children }: ChildrenType) => {
+	const pathname = usePathname();
 	return (
 		<>
-			<Header />
-			{children}
-			<Toaster />
-			<Footer />
+			{pathname.includes("dashboard") ? (
+				<div className="w-full min-h-screen flex">
+					<Sidenav />
+					{children}
+				</div>
+			) : (
+				<>
+					<Header />
+					{children}
+					<Toaster />
+					<Footer />
+				</>
+			)}
 		</>
 	);
 };
