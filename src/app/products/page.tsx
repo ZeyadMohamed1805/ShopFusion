2b7bc22d061540a92781346d8c7937f613bc5d86
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 03:35:38 +0300

    Added Form Component To The Products Page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,9 @@
+import Form from "@/components/products/form";
+
 const Products = () => (
-	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center items-center">
+	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center">
 		<div className="w-full max-w-[1400px] flex flex-col items-center">
-			<h1 className="text-3xl fw-bolder">Products</h1>
+			<Form />
 		</div>
 	</main>
 );
