df3a0d0456d891d411f610591ea2d4e38fbf154f
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 12:40:16 +0300

    Added The List Component To The Cart Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,9 @@
+import List from "@/components/cart/list";
+
 const Cart = () => (
-	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center items-center">
-		<div className="w-full max-w-[1400px] flex flex-col items-center">
-			<h1 className="text-3xl fw-bolder">Cart</h1>
+	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center">
+		<div className="w-full max-w-[1400px] flex flex-col items-center gap-8">
+			<List />
 		</div>
 	</main>
 );
