0030028b07168d7f0184fd314a0367b267c3e4a9
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 18:10:37 +0300

    Completed The Remove Cart Item Feature

--- src/components/cart/list.tsx
+++ src/components/cart/list.tsx
@@ -16,15 +16,45 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { getLocalStorageItem } from "@/utils/storage";
 import { removeCartItem, updateCartItemAmount } from "@/utils/cart";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 
 const List = () => {
 	const [cartItems, setCartItem] = useState<any[]>([]);
+	const { push } = useRouter();
+	const { toast } = useToast();
+
+	const showToast = (
+		title: any,
+		description: string,
+		action: string,
+		destructive: boolean = false
+	) => {
+		toast({
+			title: title,
+			description: description,
+			variant: destructive ? "destructive" : "default",
+			action: <ToastAction altText="Can't wait!">{action}</ToastAction>,
+		});
+	};
 
 	useEffect(() => {
 		bindCartItems();
@@ -52,86 +82,165 @@ const List = () => {
 
 	return (
 		<>
-			<Table>
-				<TableHeader>
-					<TableRow>
-						<TableHead>{""}</TableHead>
-						<TableHead>Name</TableHead>
-						<TableHead>Price</TableHead>
-						<TableHead>Amount</TableHead>
-						<TableHead className="text-right">Total</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{cartItems.map((item) => (
-						<TableRow key={item.ProductId}>
-							<TableCell className="text-right w-8">
-								<Button
-									variant="destructive"
-									className="h-8 w-8 p-0"
-									onClick={() =>
-										onDeleteCartItem(item.ProductId)
-									}
-								>
-									<Trash className="h-4 w-4" />
-								</Button>
-							</TableCell>
-							<TableCell className="font-medium whitespace-nowrap">
-								{item.ProductName}
-							</TableCell>
-							<TableCell>${item.ProductPrice}</TableCell>
-							<TableCell>
-								<Select
-									onValueChange={(value) =>
-										onSelectAmount(
-											item.ProductId,
-											parseInt(value)
-										)
-									}
-								>
-									<SelectTrigger>
-										<SelectValue placeholder="1" />
-									</SelectTrigger>
-									<SelectContent>
-										{Array.from({
-											length: item.ProductQuantityInStock,
-										}).map((_, index) => (
-											<SelectItem
-												key={index}
-												value={(index + 1).toString()}
+			{!cartItems.length ? (
+				<div className="w-full h-full text-center flex flex-col items-center justify-center gap-2">
+					<h1 className="text-3xl fw-bolder">Your Cart Is Empty.</h1>
+					<Button onClick={() => push("products")}>
+						Browse Products.
+					</Button>
+				</div>
+			) : (
+				<>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>{""}</TableHead>
+								<TableHead>Name</TableHead>
+								<TableHead>Price</TableHead>
+								<TableHead>Amount</TableHead>
+								<TableHead className="text-right">
+									Total
+								</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{!cartItems.length ? (
+								<TableRow>
+									<TableCell
+										colSpan={5}
+										className="text-center py-56"
+									>
+										<div className="flex flex-col items-center gap-2">
+											<h1 className="text-3xl fw-bolder">
+												Your Cart Is Empty.
+											</h1>
+											<Button
+												onClick={() => push("products")}
 											>
-												{index + 1}
-											</SelectItem>
-										))}
-									</SelectContent>
-								</Select>
-							</TableCell>
-							<TableCell className="text-right">
-								{item.ProductPrice * item.ProductAmount}
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-				<TableFooter>
-					<TableRow>
-						<TableCell />
-						<TableCell colSpan={3}>Total</TableCell>
-						<TableCell className="text-right">
-							{!cartItems.length
-								? "Loading..."
-								: cartItems
-										.map(
-											(item) =>
-												item.ProductPrice *
-												item.ProductAmount
-										)
-										.reduce((one, two) => one + two)}
-						</TableCell>
-					</TableRow>
-				</TableFooter>
-			</Table>
-			<Separator />
-			<Button className="w-full">Checkout</Button>
+												Browse Products.
+											</Button>
+										</div>
+									</TableCell>
+								</TableRow>
+							) : (
+								cartItems.map((item) => (
+									<TableRow key={item.ProductId}>
+										<TableCell className="text-right w-8">
+											<AlertDialog>
+												<AlertDialogTrigger asChild>
+													<Button
+														variant="destructive"
+														className="h-8 max-h-8 w-8 max-w-8 p-0"
+													>
+														<Trash className="h-4 w-4 max-h-4 max-w-4" />
+													</Button>
+												</AlertDialogTrigger>
+												<AlertDialogContent>
+													<AlertDialogHeader>
+														<AlertDialogTitle>
+															Are you sure?
+														</AlertDialogTitle>
+														<AlertDialogDescription>
+															The only way to add
+															this item back to
+															cart is by searching
+															for it again.
+														</AlertDialogDescription>
+													</AlertDialogHeader>
+													<AlertDialogFooter>
+														<AlertDialogCancel>
+															Cancel
+														</AlertDialogCancel>
+														<AlertDialogAction
+															onClick={() =>
+																onDeleteCartItem(
+																	item.ProductId
+																)
+															}
+														>
+															Confirm
+														</AlertDialogAction>
+													</AlertDialogFooter>
+												</AlertDialogContent>
+											</AlertDialog>
+										</TableCell>
+										<TableCell className="font-medium whitespace-nowrap">
+											{item.ProductName}
+										</TableCell>
+										<TableCell>
+											${item.ProductPrice}
+										</TableCell>
+										<TableCell>
+											<Select
+												onValueChange={(value) =>
+													onSelectAmount(
+														item.ProductId,
+														parseInt(value)
+													)
+												}
+											>
+												<SelectTrigger>
+													<SelectValue placeholder="1" />
+												</SelectTrigger>
+												<SelectContent>
+													{Array.from({
+														length: item.ProductQuantityInStock,
+													}).map((_, index) => (
+														<SelectItem
+															key={index}
+															value={(
+																index + 1
+															).toString()}
+														>
+															{index + 1}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+										</TableCell>
+										<TableCell className="text-right">
+											{item.ProductPrice *
+												item.ProductAmount}
+										</TableCell>
+									</TableRow>
+								))
+							)}
+						</TableBody>
+						<TableFooter>
+							<TableRow>
+								<TableCell />
+								<TableCell colSpan={3}>Total</TableCell>
+								<TableCell className="text-right">
+									{!cartItems.length
+										? 0
+										: cartItems
+												.map(
+													(item) =>
+														item.ProductPrice *
+														item.ProductAmount
+												)
+												.reduce(
+													(one, two) => one + two
+												)}
+								</TableCell>
+							</TableRow>
+						</TableFooter>
+					</Table>
+					<Separator />
+					<Button
+						className="w-full"
+						onClick={() =>
+							showToast(
+								"Checkout Items",
+								"Feature will be added soon...",
+								"Can't wait!"
+							)
+						}
+					>
+						Checkout
+					</Button>
+				</>
+			)}
 		</>
 	);
 };
