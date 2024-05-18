462a7a1f048abe8cc860b386d2eec97e4d381f1c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 04:03:52 +0300

    Added The List Component To The Products Page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,11 @@
 import Form from "@/components/products/form";
+import List from "@/components/products/list";
 
 const Products = () => (
 	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center">
 		<div className="w-full max-w-[1400px] flex flex-col items-center">
 			<Form />
+			<List />
 		</div>
 	</main>
 );
