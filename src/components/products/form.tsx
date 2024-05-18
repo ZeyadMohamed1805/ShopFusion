6e422851ba211e6300fc4c32563a38ed3cc5b44d
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 03:59:36 +0300

    Completed The Form Component's Template

--- src/components/products/form.tsx
+++ src/components/products/form.tsx
@@ -1,7 +1,36 @@
 import { Input } from "../ui/input";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 const Form = () => {
-	return <form></form>;
+	return (
+		<form className="w-full flex max-md:flex-col items-center justify-between gap-4">
+			<div className="w-full flex items-center gap-4">
+				<Input placeholder="Product Name" type="text" />
+				<Input placeholder="Product Price" type="number" />
+			</div>
+			<Select>
+				<SelectTrigger className="w-full max-w-[180px] max-md:max-w-none">
+					<SelectValue placeholder="Select Category" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectGroup>
+						<SelectLabel>Categories</SelectLabel>
+						<SelectItem value="Clothes">Clothes</SelectItem>
+						<SelectItem value="Toys">Toys</SelectItem>
+						<SelectItem value="Electronics">Electronics</SelectItem>
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</form>
+	);
 };
 
 export default Form;
