ab2b3cc7aa6b29d1ac65e9561d13f44143ca4b8c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 14:13:44 +0300

    Completed The Cart Item Operations

---
+++ src/utils/cart.ts
@@ -0,0 +1,58 @@
+import { getLocalStorageItem, setLocalStorageItem } from "./storage";
+
+export const addCartItem = (product: any): boolean => {
+	const cartItems: any[] | null = getLocalStorageItem("shop-fusion-cart");
+
+	if (cartItems) {
+		const productIndex = cartItems.findIndex(
+			(item) => item.id === product.id
+		);
+
+		if (productIndex === -1) {
+			const newCartItems = [...cartItems, product];
+			setLocalStorageItem("shop-fusion-cart", newCartItems);
+			return true;
+		}
+
+		return false;
+	} else {
+		setLocalStorageItem("shop-fusion-cart", [product]);
+		return true;
+	}
+};
+
+export const updateCartItemAmount = (
+	productId: number,
+	amount: number
+): boolean => {
+	const cartItems: any[] | null = getLocalStorageItem("shop-fusion-cart");
+
+	if (cartItems) {
+		const newCartItems = cartItems.map((item) => {
+			if (item.id === productId) {
+				item.amount = amount;
+				return item;
+			}
+			return item;
+		});
+		setLocalStorageItem("shop-fusion-cart", newCartItems);
+		return true;
+	}
+
+	return false;
+};
+
+export const removeCartItem = (productId: number): boolean => {
+	const cartItems: any[] | null = getLocalStorageItem("shop-fusion-cart");
+
+	if (cartItems) {
+		const productIndex: number = cartItems.findIndex(
+			(item) => item.id === productId
+		);
+		cartItems.splice(productIndex, 1);
+		setLocalStorageItem("shop-fusion-cart", cartItems);
+		return true;
+	}
+
+	return false;
+};
