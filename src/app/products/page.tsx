6335fd1bb6a520284474738bcd9277bf4249f28d
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 04:59:29 +0300

    Completed The Products Page's Pagination Component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import List from "@/components/products/list";
 
 const Products = () => (
 	<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center">
-		<div className="w-full max-w-[1400px] flex flex-col items-center gap-12">
+		<div className="w-full max-w-[1400px] flex flex-col items-center gap-8">
 			<Form />
 			<List />
 		</div>
--- src/components/products/list.tsx
+++ src/components/products/list.tsx
@@ -1,16 +1,51 @@
 import ProductCard from "./productCard";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const List = () => {
 	return (
-		<div
-			className="w-full grid gap-4"
-			style={{
-				gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
-			}}
-		>
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
+		<div className="w-full flex flex-col gap-8">
+			<div
+				className="w-full grid gap-4"
+				style={{
+					gridTemplateColumns: "repeat(auto-fit, minmax(340px, 1fr))",
+				}}
+			>
+				{Array.from({ length: 6 }).map((_, index) => (
+					<ProductCard key={index} />
+				))}
+			</div>
+			<div>
+				<Pagination>
+					<PaginationContent>
+						<PaginationItem className="cursor-pointer">
+							<PaginationPrevious />
+						</PaginationItem>
+						<PaginationItem className="cursor-pointer">
+							<PaginationLink>1</PaginationLink>
+						</PaginationItem>
+						<PaginationItem className="cursor-pointer">
+							<PaginationLink isActive>2</PaginationLink>
+						</PaginationItem>
+						<PaginationItem className="cursor-pointer">
+							<PaginationLink>3</PaginationLink>
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationEllipsis />
+						</PaginationItem>
+						<PaginationItem className="cursor-pointer">
+							<PaginationNext />
+						</PaginationItem>
+					</PaginationContent>
+				</Pagination>
+			</div>
 		</div>
 	);
 };
