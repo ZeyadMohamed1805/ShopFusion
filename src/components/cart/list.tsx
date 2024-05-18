39fbd920b5896970921863f25b2bd70450d6debd
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 13:07:56 +0300

    Completed The List Component's Structure

--- src/components/cart/list.tsx
+++ src/components/cart/list.tsx
@@ -1,5 +1,71 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableFooter,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
+import { cartItems } from "@/constants/constants";
+
 const List = () => {
-	return <div>List</div>;
+	return (
+		<>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Name</TableHead>
+						<TableHead>Price</TableHead>
+						<TableHead>Amount</TableHead>
+						<TableHead className="text-right">Total</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{cartItems.map((item) => (
+						<TableRow key={item.id}>
+							<TableCell className="font-medium">
+								{item.name}
+							</TableCell>
+							<TableCell>{item.price}</TableCell>
+							<TableCell>
+								<Select>
+									<SelectTrigger>
+										<SelectValue placeholder="1" />
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem value="1">1</SelectItem>
+										<SelectItem value="2">2</SelectItem>
+										<SelectItem value="3">3</SelectItem>
+									</SelectContent>
+								</Select>
+							</TableCell>
+							<TableCell className="text-right">
+								{item.total}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+				<TableFooter>
+					<TableRow>
+						<TableCell colSpan={3}>Total</TableCell>
+						<TableCell className="text-right">$1,199.94</TableCell>
+					</TableRow>
+				</TableFooter>
+			</Table>
+			<Separator />
+			<Button className="w-full">Checkout</Button>
+		</>
+	);
 };
 
 export default List;
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,46 @@
 export const navLinks: string[] = ["Home", "Products", "Cart"];
 export const navButtons: string[] = ["Login", "Register"];
+export const cartItems: any[] = [
+	{
+		id: 0,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 1,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 2,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 3,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 4,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 5,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+	{
+		id: 6,
+		name: "Product Name",
+		price: "$199.99",
+		total: "$199.99",
+	},
+];
