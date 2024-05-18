1c8ec5f8bbb508eaf17afb8d6c4edc80629dc2ab
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-18 22:02:53 +0300

    Added The Login Form To The Footer Layout Component

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+import Login from "@/components/auth/login";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { navLinks, navButtons } from "@/constants/constants";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import { Separator } from "@/components/ui/separator";
@@ -28,20 +30,29 @@ const Footer = () => {
 				</ul>
 				<ul className="flex items-center gap-2">
 					{navButtons.map((button, index) => (
-						<li key={index}>
-							<Button
-								variant={
-									button === "Login" ? "outline" : "default"
-								}
-								className={
-									button === "Login"
-										? navigationMenuTriggerStyle()
-										: ""
-								}
-							>
-								{button}
-							</Button>
-						</li>
+						<Dialog key={index}>
+							<DialogTrigger asChild>
+								<li>
+									<Button
+										variant={
+											button === "Login"
+												? "outline"
+												: "default"
+										}
+										className={
+											button === "Login"
+												? navigationMenuTriggerStyle()
+												: ""
+										}
+									>
+										{button}
+									</Button>
+								</li>
+							</DialogTrigger>
+							<DialogContent className="sm:max-w-[425px]">
+								<Login />
+							</DialogContent>
+						</Dialog>
 					))}
 				</ul>
 			</div>
