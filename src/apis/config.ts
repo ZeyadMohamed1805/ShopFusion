c72dd362f60ce52a5f1943bbd9178147e33d939b
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-21 13:23:18 +0300

    Initialized The Basic Axios Configuration File

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const client = axios.create({
+const config = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
-export default client;
+export default config;
