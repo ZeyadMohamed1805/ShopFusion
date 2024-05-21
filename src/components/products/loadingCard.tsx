57904356ba68f643d76ecea161f831519b2acbac
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-21 22:12:49 +0300

    Initalized The Loading Card Component

---
+++ src/components/products/loadingCard.tsx
@@ -0,0 +1,13 @@
+const LoadingCard = () => {
+	return (
+		<div className="card is-loading">
+			<div className="image"></div>
+			<div className="content">
+				<h2>{""}</h2>
+				<p>{""}</p>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingCard;
