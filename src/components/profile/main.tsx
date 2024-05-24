4dc09efb8a77d41a459c656cf77b2e33b031529a
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-25 02:24:48 +0300

    Completed The Profile Page

--- src/components/profile/main.tsx
+++ src/components/profile/main.tsx
@@ -4,10 +4,22 @@ import { useEffect, useState } from "react";
 import configAxios from "@/apis/config";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
 
 const Main = () => {
 	const { push } = useRouter();
-	const [user, setUser] = useState();
+	const [user, setUser] = useState<any>();
 
 	useEffect(() => {
 		axios
@@ -34,7 +46,75 @@ const Main = () => {
 			});
 	}, [push]);
 
-	return <main>Main</main>;
+	return (
+		<main className="w-full min-h-[calc(100vh-200.2px)] px-4 py-8 flex justify-center">
+			<div className="w-full max-w-[1400px] flex flex-col items-center gap-8">
+				<div className="w-full flex items-end gap-4 flex-wrap md:flex-nowrap">
+					<Avatar className="w-[180px] h-[180px]">
+						<AvatarImage src="/profile.png" />
+						<AvatarFallback>U</AvatarFallback>
+					</Avatar>
+					<div className="w-full flex items-end justify-between gap-4 capitalize">
+						<h1 className="text-4xl font-bold">
+							{`${user?.firstName} ${user?.lastName}`}
+						</h1>
+						<Button>Update</Button>
+					</div>
+				</div>
+				<Separator />
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="w-[100px]">Data</TableHead>
+							<TableHead className="text-right">Value</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						<TableRow>
+							<TableCell className="font-medium whitespace-nowrap">
+								First Name
+							</TableCell>
+							<TableCell className="text-right capitalize">
+								{user?.firstName}
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell className="font-medium whitespace-nowrap">
+								Last Name
+							</TableCell>
+							<TableCell className="text-right capitalize">
+								{user?.lastName}
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell className="font-medium whitespace-nowrap">
+								Email
+							</TableCell>
+							<TableCell className="text-right">
+								{user?.email}
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell className="font-medium whitespace-nowrap">
+								Mobile
+							</TableCell>
+							<TableCell className="text-right">
+								{user?.mobile}
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell className="font-medium whitespace-nowrap">
+								Admin
+							</TableCell>
+							<TableCell className="text-right capitalize">
+								{user?.isAdmin.toString()}
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</div>
+		</main>
+	);
 };
 
 export default Main;
